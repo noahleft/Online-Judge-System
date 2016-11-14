4669bba5abbad96854728f24998044abf066a070
ShihFengLin <noahleft@gmail.com>
2016-11-14 23:46:42 +0800

    commit for doc

---
+++ doc/Script.js
@@ -0,0 +1,11 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='course.cgi'>Course</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='fame_hall.html'>FameHall</a></li>" +
+"<li><a href='submit_page_demo.cgi'>Submit</a></li>" +
+"<li><a href='leaderboard.cgi?idx=3'>Leaderboard</a></li>" +
+"<li><a href='last_status.cgi?idx=3'>LastUploadRecord</a></li>" +
+"<li><a href='change.cgi'>Change Name</a></li>" +
+"</ul>";
