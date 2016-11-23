1213a68b4388af2336a51e5ceea9f647c9b4b974
ShihFengLin <noahleft@gmail.com>
2016-11-23 22:41:37 +0800

    remove personal data

--- docs/Script.js
+++ docs/Script.js
@@ -2,7 +2,6 @@ document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='course.html'>Course</a></li>" +
-"<li><a href='about.html'>About</a></li>" +
 "<li><a href='fame_hall.html'>FameHall</a></li>" +
 "<li><a href='submit_page.html'>Submit</a></li>" +
 "<li><a href='leaderboard.html'>Leaderboard</a></li>" +
